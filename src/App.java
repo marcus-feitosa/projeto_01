4291863b1ef8b99018c0f38a1df1f22bf8c1bed3
marcus_feitosa <marcusfeitosa02@gmail.com>
2022-07-15 15:28:00 -0300

    Criação da MainView

---
+++ src/App.java
@@ -0,0 +1,26 @@
+
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainView.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        primaryStage.setTitle("Título top");
+        primaryStage.setScene(tela);
+        primaryStage.show();
+
+    }
+}
---
+++ src/MainViewController.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController {
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    void menuItemAboutAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuItemAboutDepartment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuItemSellerAction(ActionEvent event) {
+
+    }
+
+}
